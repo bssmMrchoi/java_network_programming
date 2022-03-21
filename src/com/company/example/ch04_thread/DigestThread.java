1c14fb03733aed19ef8b3f05a7fe905de8b83388
June <kjunw0790@gmail.com>
2022-03-21 20:56:58 +0900

    chapter04 edit comment

--- src/com/company/example/ch04_thread/DigestThread.java
+++ src/com/company/example/ch04_thread/DigestThread.java
@@ -4,14 +4,15 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /*
-    Message-Digest algorithm 5
-    임의의 길이의 메시지를 입력받아, 128bit 짜리 고정 길이의 값을 출력
+    runnable 사용
+    sha-256
+    임의의 길이의 메시지를 입력받아, 256bit(64개의 16진수-4bit) 짜리 고정 길이의 값을 출력
     단방향 암호화 : 한번 암호화 되면 복호화 할 수 없음
     입력 메시지의 길이 제한X
-    보안 관련 용도로 권장하지X (충돌 : 입력이 다른데 암호화 된 출력이 같은 경우)
-    출력값 개수
-        md5 => 16*32개
-        sha-256 => 16*64개
+    현재 보안 관련 용도로 많이 사용되고 있음(패스워드)
+    =>출력값 개수
+        md5 => 2^128
+        sha-256 => 2^256
  */
 public class DigestThread implements Runnable{
     private String pwd;
--- src/com/company/example/ch04_thread/MD5.java
+++ src/com/company/example/ch04_thread/MD5.java
@@ -5,13 +5,13 @@ import java.security.NoSuchAlgorithmException;
 
 /*
     Message-Digest algorithm 5
-    임의의 길이의 메시지를 입력받아, 128bit 짜리 고정 길이의 값을 출력
+    임의의 길이의 메시지를 입력받아, 128bit(32개의 16진수-4bit) 짜리 고정 길이의 값을 출력
     단방향 암호화 : 한번 암호화 되면 복호화 할 수 없음
     입력 메시지의 길이 제한X
     보안 관련 용도로 권장하지X (충돌 : 입력이 다른데 암호화 된 출력이 같은 경우)
-    출력값 개수
-        md5 => 16*32개
-        sha-256 => 16*64개
+    =>출력값 개수
+        md5 => 2^128
+        sha-256 => 2^256
  */
 public class MD5 {
     public static String testMD5(String pwd) {
