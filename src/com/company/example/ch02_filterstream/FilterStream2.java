06f66152e68281e72f9b6d8c0540968a66039c2f
June <kjunw0790@gmail.com>
2022-03-21 20:38:47 +0900

    chapter02,03 annotate

--- src/com/company/example/ch02_filterstream/FilterStream2.java
+++ src/com/company/example/ch02_filterstream/FilterStream2.java
@@ -1,17 +1,25 @@
 package com.company.example.ch02_filterstream;
 
 import java.io.*;
-
+/*
+    파일 생성 및 데이터 저장 후 파일 읽기
+    BufferedOutputStream : 파일을 기록할 때 8192byte의 버퍼를 두고 작업 -> 속도 향상
+    DataOutputStream : 자바의 기본 데이터 타입별로 출력하는 메소드 -> 바이너리 형태로 저장
+ */
 public class FilterStream2 {
     public static void main(String[] args) {
         try {
             DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("data.txt")));
             dout.writeInt(65);
             //00000000 00000000 00000000 01000001 저장
+            dout.writeUTF("안녕?");
+            dout.writeChar('c');
             dout.close();
 
             DataInputStream din = new DataInputStream(new BufferedInputStream(new FileInputStream("data.txt")));
             System.out.println(din.readInt());
+            System.out.println(din.readUTF());
+            System.out.println(din.readChar());
             din.close();
         } catch (IOException e) {
             e.printStackTrace();
--- src/com/company/example/ch03_readerwriter/ReaderWriterStream.java
+++ src/com/company/example/ch03_readerwriter/ReaderWriterStream.java
@@ -2,15 +2,21 @@ package com.company.example.ch03_readerwriter;
 
 import java.io.*;
 
+/*
+    키보드로 입력한 데이터를 파일에 저장 후 파일 읽기
+ */
 public class ReaderWriterStream {
     public static void main(String[] args) {
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String data = reader.readLine();
-            System.out.println(data);
 
             BufferedWriter writer = new BufferedWriter(new FileWriter("data.txt"));
             writer.write(data);
             writer.close();
+
+            BufferedReader readFile = new BufferedReader(new FileReader("data.txt"));
+            System.out.println(readFile.readLine());
+            readFile.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
